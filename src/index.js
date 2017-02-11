6a11305b8a36712112a561f169c19c8674b1f551
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2017-02-11 17:17:36 +0100

    Initial commit

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import Root from './containers/Root'
-
-// Rendering our Root component in #root HTML element
-ReactDom.render(<Root />, document.getElementById('root'))
