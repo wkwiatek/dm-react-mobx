813911af9517b60e868620e4cc0d0f8935443692
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2017-02-13 08:51:50 +0100

    Getting data from a state of a component [State]

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,27 @@
 import React from 'react'
 
-//6/ Let's start with some stateless mocked component
-const ProductList = () => (
-  <ul>
-    <li>Apple</li>
-    <li>Coffee</li>
-  </ul>
-)
+// We don't have stateless component any more, so we do have to use classes
+class ProductList extends React.Component {
+  //6/ Class properties are one of the nicest way of dealing with an initial state
+  state = {
+    products: [
+      { id: 0, name: 'Apple' },
+      { id: 1, name: 'Coffee' }
+    ]
+  }
 
-// End expose it
+  render() {
+    //9/ We're mapping component's state into a view
+    return (
+      <ul>
+        { this.state.products.map(p => (
+          <li key={ p.id }>
+            { p.name }
+          </li>
+        ) ) }
+      </ul>
+    )
+  }
+}
+
 export default ProductList
--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,8 +1,6 @@
 import React from 'react'
-// Importing product list component from newly created 'components' directory
 import ProductList from '../components/ProductList'
 
-//3/ Our root component now is rendering the product list component
 const Root = () => (
   <ProductList />
 )
