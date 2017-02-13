880f4304c5779a08c95046742cb0f59d0c05bda6
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2017-02-13 09:16:19 +0100

    Adding button to buy a product [Event handler]

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
-//5/ As our product will grow, it'd be better to extract is into its own component
-const Product = ({ name }) => (
-  <span>
-    { name }
-  </span>
-)
+// Now we have to pass also id, isSold, and onBuyClick handler
+const Product = ({ id, name, isSold, onBuyClick }) => {
+  //3/ Here we need an id to do a proper callback
+  const handleClick = () => {
+    onBuyClick(id)
+  }
+
+  return (
+    <div>
+      {/* Here we can add some inline styling to see component changes on the UI */}
+      <span style={ { textDecoration: isSold ? 'line-through' : 'none' }}>
+        { name }
+      </span>
+      {/* And that's how our handler looks like */}
+      <button onClick={ handleClick }>
+        Buy
+      </button>
+    </div>
+  )
+}
 
 export default Product
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,11 +2,25 @@ import React from 'react'
 import Product from './Product'
 
 class ProductList extends React.Component {
+
+  //6/ isSold is a new property in our state
   state = {
     products: [
-      { id: 0, name: 'Apple' },
-      { id: 1, name: 'Coffee' }
+      { id: 0, name: 'Apple', isSold: false },
+      { id: 1, name: 'Coffee', isSold: false }
     ]
+  }
+
+  //10/ Here is the real handler that changes the state
+  handleBuyClick = id => {
+    this.setState(previousState => {
+      const products = previousState.products.map(p => p.id === id
+        ? { ...p, isSold: true }
+        : p
+      )
+
+      return { products }
+    })
   }
 
   render() {
@@ -14,8 +28,13 @@ class ProductList extends React.Component {
       <ul>
         { this.state.products.map(p => (
           <li key={ p.id }>
-            {/* We can make a product component instead of putting more and more data here */}
-            <Product name={ p.name } />
+            {/* Here we're passing more data */}
+            <Product
+              id={ p.id }
+              name={ p.name }
+              isSold={ p.isSold }
+              onBuyClick={ this.handleBuyClick }
+            />
           </li>
         ) ) }
       </ul>
